22eac779aebacaa2849cd8cd25f101d0f3d0d523
Abel Alan <32365395+abelZW@users.noreply.github.com>
2019-02-22 17:25:55 +0800

    Create 3.5.cpp

---
+++ 3/3.5.cpp
@@ -0,0 +1,12 @@
+//chartype.cpp -- the char type
+#include <iostream>
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    char ch;            //declar a char variable
+
+    cout << "Enter a character: " << endl;
+    cin >> ch;
+    cout << "Holla! ";
+    cout << "Thank you for the " << ch << "character." << endl;
+    return 0;
+}
