ae61b4338a7e5d43d92d992e09e03ef897938849
Abel Alan <32365395+abelZW@users.noreply.github.com>
2019-02-22 17:19:54 +0800

    Update 3.4.cpp

--- 3/3.4.cpp
+++ 3/3.4.cpp
@@ -1,14 +1,16 @@
-//hexoct.cpp -- shows hex and octal constants
+//hexoct2.cpp -- display values in hex and octal
 #include <iostream>
 int main(int argc, char const *argv[]) {
     using namespace std;
-    int chest = 42;     //decimal integer constants
-    int waist = 0x42;   //hexadeciaml integer constants
-    int inseam = 042;   //octal integer constants
+    int chest = 42;
+    int waist = 42;
+    int inseam = 42;
 
     cout << "monsieur cuts a striking figure!" << endl;
-    cout << "chest = " << chest << "\n";
-    cout << "waist = " << waist << endl;
-    cout << "inseam = " << inseam << endl;
+    cout << "chest = " << chest << " (decimal)\n";
+    cout << hex;            //manipulator for changing number base
+    cout << "waist = " << waist << " (hexadecimal)" << endl;
+    cout << oct;            //mainpulator for changing number base
+    cout << "inseam = " << inseam << " (octal)"<< endl;
     return 0;
 }
